a35575746e4fa013cdf9cedfa392a8b7132ea60f
Zagal <zagalfayeezullah@gmail.com>
2022-10-30 00:46:50 +0600

    3rd commit

--- pages/tests.py
+++ pages/tests.py
@@ -8,3 +8,4 @@ class SimpleTests(SimpleTestCase):
     def test_about_page_status_code(self):
         response = self.client.get('/about/')
         self.assertEqual(response.status_code, 200)
+#NEWCODE
